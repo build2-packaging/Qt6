83edd81287c4189b6e4b7e6390f8e74557d705b5
Francois Kritzinger <francois@codesynthesis.com>
2022-12-12 14:52:29 +0200

    Build libQt6Widgets

---
+++ libQt6Widgets/QtWidgets/qtwidgets-config.h
@@ -0,0 +1,325 @@
+#pragma once
+
+#define QT_FEATURE_abstractbutton 1
+#define QT_FEATURE_abstractslider 1
+
+#define QT_FEATURE_buttongroup 1
+#if !QT_CONFIG(buttongroup)
+#  define QT_NO_BUTTONGROUP 1
+#endif
+
+#define QT_FEATURE_calendarwidget 1
+#if !QT_CONFIG(calendarwidget)
+#  define QT_NO_CALENDARWIDGET 1
+#endif
+
+#define QT_FEATURE_checkbox 1
+
+#define QT_FEATURE_colordialog 1
+#if !QT_CONFIG(colordialog)
+#  define QT_NO_COLORDIALOG 1
+#endif
+
+#define QT_FEATURE_columnview 1
+#if !QT_CONFIG(columnview)
+#  define QT_NO_COLUMNVIEW 1
+#endif
+
+#define QT_FEATURE_combobox 1
+#if !QT_CONFIG(combobox)
+#  define QT_NO_COMBOBOX 1
+#endif
+
+#define QT_FEATURE_commandlinkbutton 1
+
+#define QT_FEATURE_completer 1
+#if !QT_CONFIG(completer)
+#  define QT_NO_COMPLETER 1
+#endif
+
+#define QT_FEATURE_contextmenu 1
+#if !QT_CONFIG(contextmenu)
+#  define QT_NO_CONTEXTMENU 1
+#endif
+
+#define QT_FEATURE_datawidgetmapper 1
+#if !QT_CONFIG(datawidgetmapper)
+#  define QT_NO_DATAWIDGETMAPPER 1
+#endif
+
+#define QT_FEATURE_datetimeedit 1
+#if !QT_CONFIG(datetimeedit)
+#  define QT_NO_DATETIMEEDIT 1
+#endif
+
+#define QT_FEATURE_dial 1
+#if !QT_CONFIG(dial)
+#  define QT_NO_DIAL 1
+#endif
+
+#define QT_FEATURE_dialog 1
+#define QT_FEATURE_dialogbuttonbox 1
+
+#define QT_FEATURE_dockwidget 1
+#if !QT_CONFIG(dockwidget)
+#  define QT_NO_DOCKWIDGET 1
+#endif
+
+#define QT_FEATURE_errormessage 1
+#if !QT_CONFIG(errormessage)
+#  define QT_NO_ERRORMESSAGE 1
+#endif
+
+#define QT_FEATURE_filedialog 1
+#if !QT_CONFIG(filedialog)
+#  define QT_NO_FILEDIALOG 1
+#endif
+
+#define QT_FEATURE_fontcombobox 1
+#if !QT_CONFIG(fontcombobox)
+#  define QT_NO_FONTCOMBOBOX 1
+#endif
+
+#define QT_FEATURE_fontdialog 1
+#if !QT_CONFIG(fontdialog)
+#  define QT_NO_FONTDIALOG 1
+#endif
+
+#define QT_FEATURE_formlayout 1
+
+#define QT_FEATURE_fscompleter 1
+#if !QT_CONFIG(fscompleter)
+#  define QT_NO_FSCOMPLETER 1
+#endif
+
+#define QT_FEATURE_graphicseffect 1
+#if !QT_CONFIG(graphicseffect)
+#  define QT_NO_GRAPHICSEFFECT 1
+#endif
+
+#define QT_FEATURE_graphicsview 1
+#if !QT_CONFIG(graphicsview)
+#  define QT_NO_GRAPHICSVIEW 1
+#endif
+
+#define QT_FEATURE_groupbox 1
+#if !QT_CONFIG(groupbox)
+#  define QT_NO_GROUPBOX 1
+#endif
+
+#define QT_FEATURE_inputdialog 1
+#if !QT_CONFIG(inputdialog)
+#  define QT_NO_INPUTDIALOG 1
+#endif
+
+#define QT_FEATURE_itemviews 1
+#if !QT_CONFIG(itemviews)
+#  define QT_NO_ITEMVIEWS 1
+#endif
+
+#define QT_FEATURE_keysequenceedit 1
+#if !QT_CONFIG(keysequenceedit)
+#  define QT_NO_KEYSEQUENCEEDIT 1
+#endif
+
+#define QT_FEATURE_label 1
+
+#define QT_FEATURE_lcdnumber 1
+#if !QT_CONFIG(lcdnumber)
+#  define QT_NO_LCDNUMBER 1
+#endif
+
+#define QT_FEATURE_lineedit 1
+#if !QT_CONFIG(lineedit)
+#  define QT_NO_LINEEDIT 1
+#endif
+
+#define QT_FEATURE_listview 1
+#if !QT_CONFIG(listview)
+#  define QT_NO_LISTVIEW 1
+#endif
+
+#define QT_FEATURE_listwidget 1
+#if !QT_CONFIG(listwidget)
+#  define QT_NO_LISTWIDGET 1
+#endif
+
+#define QT_FEATURE_mainwindow 1
+#if !QT_CONFIG(mainwindow)
+#  define QT_NO_MAINWINDOW 1
+#endif
+
+#define QT_FEATURE_mdiarea 1
+#if !QT_CONFIG(mdiarea)
+#  define QT_NO_MDIAREA 1
+#endif
+
+#define QT_FEATURE_menu 1
+#if !QT_CONFIG(menu)
+#  define QT_NO_MENU 1
+#endif
+
+#define QT_FEATURE_menubar 1
+#if !QT_CONFIG(menubar)
+#  define QT_NO_MENUBAR 1
+#endif
+
+#define QT_FEATURE_messagebox 1
+#if !QT_CONFIG(messagebox)
+#  define QT_NO_MESSAGEBOX 1
+#endif
+
+#define QT_FEATURE_progressbar 1
+#if !QT_CONFIG(progressbar)
+#  define QT_NO_PROGRESSBAR 1
+#endif
+
+#define QT_FEATURE_progressdialog 1
+#if !QT_CONFIG(progressdialog)
+#  define QT_NO_PROGRESSDIALOG 1
+#endif
+
+#define QT_FEATURE_pushbutton 1
+#define QT_FEATURE_radiobutton 1
+
+#define QT_FEATURE_resizehandler 1
+#if !QT_CONFIG(resizehandler)
+#  define QT_NO_RESIZEHANDLER 1
+#endif
+
+#define QT_FEATURE_rubberband 1
+#if !QT_CONFIG(rubberband)
+#  define QT_NO_RUBBERBAND 1
+#endif
+
+#define QT_FEATURE_scrollarea 1
+#if !QT_CONFIG(scrollarea)
+#  define QT_NO_SCROLLAREA 1
+#endif
+
+#define QT_FEATURE_scrollbar 1
+#if !QT_CONFIG(scrollbar)
+#  define QT_NO_SCROLLBAR 1
+#endif
+
+#define QT_FEATURE_scroller 1
+
+#define QT_FEATURE_sizegrip 1
+#if !QT_CONFIG(sizegrip)
+#  define QT_NO_SIZEGRIP 1
+#endif
+
+#define QT_FEATURE_slider 1
+#if !QT_CONFIG(slider)
+#  define QT_NO_SLIDER 1
+#endif
+
+#define QT_FEATURE_spinbox 1
+#if !QT_CONFIG(spinbox)
+#  define QT_NO_SPINBOX 1
+#endif
+
+#define QT_FEATURE_splashscreen 1
+#if !QT_CONFIG(splashscreen)
+#  define QT_NO_SPLASHSCREEN 1
+#endif
+
+#define QT_FEATURE_splitter 1
+#if !QT_CONFIG(splitter)
+#  define QT_NO_SPLITTER 1
+#endif
+
+#define QT_FEATURE_stackedwidget 1
+#if !QT_CONFIG(stackedwidget)
+#  define QT_NO_STACKEDWIDGET 1
+#endif
+
+#define QT_FEATURE_statusbar 1
+#if !QT_CONFIG(statusbar)
+#  define QT_NO_STATUSBAR 1
+#endif
+
+#define QT_FEATURE_statustip 1
+#if !QT_CONFIG(statustip)
+#  define QT_NO_STATUSTIP 1
+#endif
+
+#define QT_FEATURE_style_stylesheet 1
+#if !QT_CONFIG(style_stylesheet)
+#  define QT_NO_STYLE_STYLESHEET 1
+#endif
+
+#define QT_FEATURE_syntaxhighlighter 1
+#if !QT_CONFIG(syntaxhighlighter)
+#  define QT_NO_SYNTAXHIGHLIGHTER 1
+#endif
+
+#define QT_FEATURE_tabbar 1
+#if !QT_CONFIG(tabbar)
+#  define QT_NO_TABBAR 1
+#endif
+
+#define QT_FEATURE_tableview 1
+#if !QT_CONFIG(tableview)
+#  define QT_NO_TABLEVIEW 1
+#endif
+
+#define QT_FEATURE_tablewidget 1
+#if !QT_CONFIG(tablewidget)
+#  define QT_NO_TABLEWIDGET 1
+#endif
+
+#define QT_FEATURE_tabwidget 1
+#if !QT_CONFIG(tabwidget)
+#  define QT_NO_TABWIDGET 1
+#endif
+
+#define QT_FEATURE_textbrowser 1
+#if !QT_CONFIG(textbrowser)
+#  define QT_NO_TEXTBROWSER 1
+#endif
+
+#define QT_FEATURE_textedit 1
+#if !QT_CONFIG(textedit)
+#  define QT_NO_TEXTEDIT 1
+#endif
+
+#define QT_FEATURE_toolbar 1
+#if !QT_CONFIG(toolbar)
+#  define QT_NO_TOOLBAR 1
+#endif
+
+#define QT_FEATURE_toolbox 1
+#if !QT_CONFIG(toolbox)
+#  define QT_NO_TOOLBOX 1
+#endif
+
+#define QT_FEATURE_toolbutton 1
+#if !QT_CONFIG(toolbutton)
+#  define QT_NO_TOOLBUTTON 1
+#endif
+
+#define QT_FEATURE_tooltip 1
+#if !QT_CONFIG(tooltip)
+#  define QT_NO_TOOLTIP 1
+#endif
+
+#define QT_FEATURE_treeview 1
+#if !QT_CONFIG(treeview)
+#  define QT_NO_TREEVIEW 1
+#endif
+
+#define QT_FEATURE_treewidget 1
+#if !QT_CONFIG(treewidget)
+#  define QT_NO_TREEWIDGET 1
+#endif
+
+#define QT_FEATURE_undoview 1
+#if !QT_CONFIG(undoview)
+#  define QT_NO_UNDOVIEW 1
+#endif
+
+#define QT_FEATURE_wizard 1
+#if !QT_CONFIG(wizard)
+#  define QT_NO_WIZARD 1
+#endif
