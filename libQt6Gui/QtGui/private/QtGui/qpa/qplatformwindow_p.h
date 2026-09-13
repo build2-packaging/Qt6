ee63c867ccf072f17bdcb4ba07f3129108665c38
William Roy <wroy@proton.me>
2026-09-13 10:17:10 -0400

    Generated changes (modified upstream sources)

--- libQt6Gui/QtGui/private/QtGui/qpa/qplatformwindow_p.h
+++ libQt6Gui/QtGui/private/QtGui/qpa/qplatformwindow_p.h
@@ -28,6 +28,14 @@
 struct wl_surface;
 #endif
 
+#if defined(Q_OS_MACOS)
+Q_FORWARD_DECLARE_OBJC_CLASS(CALayer);
+typedef long NSInteger;
+enum NSVisualEffectMaterial : NSInteger;
+enum NSVisualEffectBlendingMode : NSInteger;
+enum NSVisualEffectState: NSInteger;
+#endif
+
 QT_BEGIN_NAMESPACE
 
 class QMargins;
@@ -55,9 +63,13 @@ struct Q_GUI_EXPORT QWasmWindow
 #if defined(Q_OS_MACOS) || defined(Q_QDOC)
 struct Q_GUI_EXPORT QCocoaWindow
 {
-    QT_DECLARE_NATIVE_INTERFACE(QCocoaWindow, 1, QWindow)
+    QT_DECLARE_NATIVE_INTERFACE(QCocoaWindow, 2, QWindow)
     virtual void setContentBorderEnabled(bool enable) = 0;
     virtual QPoint bottomLeftClippedByNSWindowOffset() const = 0;
+    virtual CALayer *contentLayer() const = 0;
+    virtual void manageVisualEffectArea(quintptr identifier, const QRect &rect,
+        NSVisualEffectMaterial material, NSVisualEffectBlendingMode blendMode,
+        NSVisualEffectState activationState) = 0;
 };
 #endif
 
@@ -113,6 +125,14 @@ struct Q_GUI_EXPORT QWaylandWindow : public QObject
 public:
     QT_DECLARE_NATIVE_INTERFACE(QWaylandWindow, 1, QWindow)
 
+    enum WindowType {
+        Default,
+        ToolTip,
+        ComboBox,
+        Menu,
+        SubMenu,
+    };
+
     virtual wl_surface *surface() const = 0;
     virtual void setCustomMargins(const QMargins &margins) = 0;
     virtual void requestXdgActivationToken(uint serial) = 0;
@@ -123,9 +143,16 @@ public:
         auto role = std::any_cast<T *>(&anyRole);
         return role ? *role : nullptr;
     }
+    virtual void setSessionRestoreId(const QString &role) = 0;
+
+    virtual void setExtendedWindowType(WindowType windowType) = 0;
+    virtual void setParentControlGeometry(const QRect &parentAnchor) = 0;
+
 Q_SIGNALS:
     void surfaceCreated();
     void surfaceDestroyed();
+    void surfaceRoleCreated();
+    void surfaceRoleDestroyed();
     void xdgActivationTokenCreated(const QString &token);
 
 protected:
