8905be7d96c53d9450f9a410a0226149fca790ae
William Roy <wroy@proton.me>
2026-09-13 11:27:32 -0400

    Patch qdialog.cpp

--- downstream/qtbase/src/widgets/dialogs/qdialog.cpp
+++ downstream/qtbase/src/widgets/dialogs/qdialog.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 // Qt-Security score:significant reason:default
 
-#include "kernel/qwidget_p.h"
+#include "private/qwidget_p.h"
 #include <QtWidgets/qtwidgetsglobal.h>
 #include <utility>
 #if QT_CONFIG(colordialog)
